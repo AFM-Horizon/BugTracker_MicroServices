f6f452b111d3a85abec751549ddd520aa7491580
AFMHorizon <52022448+AFM-Horizon@users.noreply.github.com>
2021-02-02 23:26:41 +1100

    Fixed issue with route

--- bug-tracker-api/routes/workspace-alt-routes.js
+++ bug-tracker-api/routes/workspace-alt-routes.js
@@ -5,6 +5,6 @@ const authenticator = require('../authentication/authenticator');
 
 const router = express.Router();
 
-router.get('/getInvitedSpaces', authenticator.authenticateToken, controller.get_invited_workspaces_by_userid);
+router.get('/getInvitedSpaces:userId', authenticator.authenticateToken, controller.get_invited_workspaces_by_userid);
 
 module.exports = router;
